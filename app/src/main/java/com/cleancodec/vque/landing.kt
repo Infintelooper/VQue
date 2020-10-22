ff308b38c9c0deb6cb5f99e4845ccbcc390d95b1
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-10-22 11:41:44 +0530

    added material component lib with error.

--- app/src/main/java/com/cleancodec/vque/landing.kt
+++ app/src/main/java/com/cleancodec/vque/landing.kt
@@ -2,20 +2,27 @@ package com.cleancodec.vque
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ListView
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_landing.*
 
 class landing : AppCompatActivity() {
+    lateinit var mToolbar:Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
 
+        mToolbar = findViewById(R.id.main_tool_bar)
+        setSupportActionBar(mToolbar)
 
-        search_bar.setOnClickListener()
-        {
+    }
 
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
     }
 }
