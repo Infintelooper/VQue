ce6e4015a6eefc11569dd5c6db47c38f8c72928f
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-10-22 09:43:13 +0530

    added material component lib

--- app/src/main/java/com/cleancodec/vque/landing.kt
+++ app/src/main/java/com/cleancodec/vque/landing.kt
@@ -2,10 +2,20 @@ package com.cleancodec.vque
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_landing.*
 
 class landing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+
+        search_bar.setOnClickListener()
+        {
+
+        }
     }
 }
