0b1ee7b9cc519fc66d61cc680ad3f7e3ba14f900
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-10-23 09:22:37 +0530

    notification panel added with click event

--- app/src/main/java/com/cleancodec/vque/landing.kt
+++ app/src/main/java/com/cleancodec/vque/landing.kt
@@ -1,11 +1,66 @@
 package com.cleancodec.vque
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_landing.*
+import kotlinx.android.synthetic.main.activity_signin.*
+
 
 class landing : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+        notification_btn.setOnClickListener(){
+            if(notification_panel.alpha == 0f){
+                notification_panel.animate()
+                    .alpha(1f)
+                    .setInterpolator(AccelerateDecelerateInterpolator()).duration = 100
+            }
+            else{
+                notification_panel.animate()
+                    .alpha(0f)
+                    .setInterpolator(AccelerateDecelerateInterpolator()).duration = 100
+            }
+        }
+        //code for popup search extended area when text changed
+        shop_search_editText.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+
+                if(shop_search_editText.length() > 0){
+                    search_bar_extended.animate()
+                        .alpha(1f)
+                        .setInterpolator(AccelerateDecelerateInterpolator()).duration = 100
+                }
+                else{
+                    search_bar_extended.animate()
+                        .alpha(0f)
+                        .setInterpolator(AccelerateDecelerateInterpolator()).duration = 100
+                }
+                search_bar_extended.setOnClickListener(){
+                    search_bar_extended.animate()
+                        .alpha(0f)
+                        .setInterpolator(AccelerateDecelerateInterpolator()).duration = 100
+                }
+
+
+
+            }
+        })
+
+
     }
 }
