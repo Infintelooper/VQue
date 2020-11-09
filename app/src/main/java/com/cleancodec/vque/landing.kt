6989007000cbb063ab7b99c068b08330e48cfe6c
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-11-09 18:59:20 +0530

    data add to firebase dialog added, not applicable right know, so disabling

--- app/src/main/java/com/cleancodec/vque/landing.kt
+++ app/src/main/java/com/cleancodec/vque/landing.kt
@@ -1,16 +1,32 @@
 package com.cleancodec.vque
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
+import android.text.InputType
 import android.text.TextWatcher
+import android.util.Log
+import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_landing.*
 import kotlinx.android.synthetic.main.activity_signin.*
+import java.util.*
+import kotlin.collections.HashMap
 
+private const val TAG:String = "FILESTORE SEARCH LOG"
 
 class landing : AppCompatActivity() {
 
+    //firebase for search
+    private val firebaseAuth:FirebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseFirestore:FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,11 +72,46 @@ class landing : AppCompatActivity() {
                         .setInterpolator(AccelerateDecelerateInterpolator()).duration = 100
                 }
 
-
-
+                //code to execute search function
+                //showAlertDialog();
             }
         })
 
+    }
+    private fun showAlertDialog(){
+        val alertDialog:AlertDialog.Builder = AlertDialog.Builder(this)
+        alertDialog.setTitle("Search Shop")
+        alertDialog.setMessage("Enter shop name here")
 
+        //add input
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        alertDialog.setView(input)
+
+         // add positive button
+         alertDialog.setPositiveButton("Conform Add"
+         ) { _, _ ->
+         //Get value from  input field
+         val inputText:String = input.text.toString()
+
+         //add data to firestore
+         addToFirestore(inputText)
+         }
+
+        //Show alert dialog
+        alertDialog.show()
+    }
+
+    private fun addToFirestore(inputText: String) {
+        val bookMap = HashMap<String,String>()
+        bookMap["title"] = inputText
+        bookMap["search_keywords"] = inputText.toLowerCase()
+
+        //add to firebase
+        firebaseFirestore.collection("Books").add(bookMap).addOnCompleteListener{
+            if(!it.isSuccessful){
+                Log.d(TAG,"Error: ${it.exception!!.message}")
+            }
+        }
     }
 }
