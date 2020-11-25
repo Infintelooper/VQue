515905c6e2dfe9ee4925e85ccc43c5dad9d2ecbb
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2020-11-25 23:42:44 +0530

    intro loading added

--- app/src/main/java/com/cleancodec/vque/Intro.kt
+++ app/src/main/java/com/cleancodec/vque/Intro.kt
@@ -4,14 +4,24 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AccelerateDecelerateInterpolator
+import kotlinx.android.synthetic.main.activity_intro.*
+import kotlinx.android.synthetic.main.activity_landing.*
 
 class Intro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
-        isAuthenticaed()
+        spin_kit_main.animate()
+            .alpha(1f)
+            .setInterpolator(AccelerateDecelerateInterpolator()).duration = 400
+        Handler().postDelayed({
+            isAuthenticaed()
+        }, 5000)
     }
     private fun isAuthenticaed(){
+
         //check authentication
         val preference=getSharedPreferences(resources.getString(R.string.app_name), Context.MODE_PRIVATE)
         if(preference.getBoolean("isAuthenticated",false))
